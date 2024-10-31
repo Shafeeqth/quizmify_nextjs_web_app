83eae07d6ee48849a9610b86cf22c4db41e37308
shafeeque <shafeeqsha06@gmail.com>
2024-10-31 13:23:38 +0530

    feat: added build in components from shadcn

---
+++ src/components/dashboard/CustomWordCloud.tsx
@@ -0,0 +1,17 @@
+import D3WordCloud from "react-d3-cloud";
+
+type Props = {}
+
+const data = [
+    {
+        text: "hello"
+    }
+]
+
+const CustomWordCloud = (props: Props) => {
+  return (
+
+  )
+}
+
+export default CustomWordCloud
